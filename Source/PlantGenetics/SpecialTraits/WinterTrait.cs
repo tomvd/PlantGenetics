1c5c36685de57c2b15f88813e881de1f40e9d197
Tom Van Dyck <tomvdtom@gmail.com>
2024-03-21 21:20:04 +0100

    v2.3.1 Fix winter trait not working

--- Source/PlantGenetics/SpecialTraits/WinterTrait.cs
+++ Source/PlantGenetics/SpecialTraits/WinterTrait.cs
@@ -23,34 +23,72 @@ public static class WinterTrait
     [HarmonyPatch(typeof(Plant), nameof(Plant.GrowthRateFactor_Temperature), MethodType.Getter)]
     public class GrowthRateFactor_Temperature
     {
-        [HarmonyPostfix]
-        public static void Postfix(ref float __result, Plant __instance)
+        [HarmonyPrefix]
+        public static bool Prefix(ref float __result, Plant __instance)
         {
             if (__instance.hasWinterTrait())
             {
-                float minTemp = -16f;
-                float maxTemp = 41f;
                 if (!GenTemperature.TryGetTemperatureForCell(__instance.Position, __instance.Map, out var tempResult))
                 {
                     __result = 1f;
                 }
                 else
                 {
+                    __result = 1f;
+                    
                     if (tempResult < -10f)
                     {
-                        __result = Mathf.InverseLerp(minTemp, -10f, tempResult);
+                        __result = Mathf.InverseLerp(-16f, -10f, tempResult);
                     }
 
                     if (tempResult > 25f)
                     {
-                        __result = Mathf.InverseLerp(maxTemp, 25f, tempResult);
+                        __result = Mathf.InverseLerp(41f, 25f, tempResult);
                     }
-
-                    __result = 1f;
                 }
+                return false;
             }
+            return true; // do the vanilla thang
         }
     }
+    
+    /*
+     * public virtual float GrowthRate
+{
+	get
+	{
+		if (Blighted)
+		{
+			return 0f;
+		}
+		if (base.Spawned && !PlantUtility.GrowthSeasonNow(base.Position, base.Map))
+		{
+			return 0f;
+		}
+		return GrowthRateFactor_Fertility * GrowthRateFactor_Temperature * GrowthRateFactor_Light * GrowthRateFactor_NoxiousHaze;
+	}
+}
+     */
+    
+    [HarmonyPatch(typeof(Plant), nameof(Plant.GrowthRate), MethodType.Getter)]
+    public class GrowthRate
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref float __result, Plant __instance)
+        {
+            if (__instance.hasWinterTrait())
+            {
+                if (__instance.Blighted)
+                {
+                    __result = 0f;
+                }
+                // removed the sowing season now requirement
+                __result = __instance.GrowthRateFactor_Fertility * __instance.GrowthRateFactor_Temperature * __instance.GrowthRateFactor_Light * __instance.GrowthRateFactor_NoxiousHaze;                
+                return false;
+            }
+            return true; // do the vanilla thang
+        }
+    }    
     
     /// <summary>
     /// Plants with wintertrait go leafless at -50
